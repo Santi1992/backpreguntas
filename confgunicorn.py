14a0773fb2eea3782a7fe7ec79aa16643a1df34f
santiago <santiagorueda@msn.com>
2022-02-23 17:38:58 -0300

    cambio puerto

--- confgunicorn.py
+++ confgunicorn.py
@@ -2,7 +2,7 @@ import multiprocessing
 import os
 
 timeout=30
-bind = '0.0.0.0:8097'
+bind = '0.0.0.0:8080'
 worker_class = 'gthread'
 max_requests = 3
 #worker_tmp_dir = "/dev/shm"  Si lo corrremos con docker
